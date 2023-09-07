818e827a1e2edf7df4bf0cc070a7ab3077a743b4
ZachMommaerts <zmommaerts@gmail.com>
2023-09-07 15:48:32 -0500

    Initial Commit

---
+++ RockPaperScissors/RockPaperScissorsApp.swift
@@ -0,0 +1,17 @@
+//
+//  RockPaperScissorsApp.swift
+//  RockPaperScissors
+//
+//  Created by Zach Mommaerts on 9/7/23.
+//
+
+import SwiftUI
+
+@main
+struct RockPaperScissorsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
