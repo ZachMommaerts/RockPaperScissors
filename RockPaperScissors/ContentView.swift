97c7b68dd3a144a608e0beafee25165c761e9ef9
ZachMommaerts <zmommaerts@gmail.com>
2023-09-11 16:53:54 -0500

    - Created functional Rock/Paper/Scissors game

--- RockPaperScissors/ContentView.swift
+++ RockPaperScissors/ContentView.swift
@@ -8,14 +8,143 @@
 import SwiftUI
 
 struct ContentView: View {
-    var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+    
+    enum Outcomes {
+        case win, lose, draw
+    }
+    
+    var options = ["🪨", "📃", "✂️"]
+    @State private var round = 1
+    @State private var playerScore = 0
+    @State private var cpuScore = 0
+    @State private var showingScore = false
+    @State private var showingFinalScore = false
+    @State private var resultsTitle = ""
+    @State private var playerChoice = "🪨"
+    @State private var computerChoice = Int.random(in: 0...2)
+    @State private var shouldWin = Bool.random()
+    
+    var outcome: Outcomes {
+        guard playerChoice != options[computerChoice] else {
+            return .draw
         }
-        .padding()
+        
+        if (playerChoice == "🪨" && options[computerChoice] == "📃") {
+            return .lose
+        } else if (playerChoice == "📃" && options[computerChoice] == "✂️") {
+            return .lose
+        } else if (playerChoice == "✂️" && options[computerChoice] == "🪨") {
+            return .lose
+        } else {
+            return .win
+        }
+    }
+    
+    var body: some View {
+        ZStack{
+            Color.blue
+            VStack {
+                Spacer()
+                Text("R/P/S")
+                    .font(.largeTitle)
+                Text("Round \(round)")
+                    .font(.title2)
+                    .padding(.vertical)
+                Section {
+                    HStack{
+                        Spacer()
+                        Text("Player Score: \(playerScore)")
+                            .font(.title3)
+                        Spacer()
+                        Text("CPU Score: \(cpuScore)")
+                            .font(.title3)
+                        Spacer()
+                    }
+                    .padding(.vertical)
+                    Text("Your opponent has chosen:")
+                    Text(options[computerChoice])
+                        .font(.system(size: 200))
+                        .padding(.vertical)
+                    Text(shouldWin ? "Select the option to win" : "Select the option to lose")
+                        .padding(.bottom)
+                }
+                HStack{
+                    Spacer()
+                    Button("🪨"){
+                        handleButtonPress(emoji: "🪨")
+                    }
+                    .font(.system(size: 50))
+        
+                    Spacer()
+                    
+                    Button("📃"){
+                        handleButtonPress(emoji: "📃")
+                    }
+                    .font(.system(size: 50))
+                    
+                    Spacer()
+                    
+                    Button("✂️"){
+                        handleButtonPress(emoji: "✂️")
+                    }
+                    .font(.system(size: 50))
+                    Spacer()
+                }
+                Spacer()
+            }
+        }
+        .foregroundColor(.white)
+        .ignoresSafeArea()
+        .alert(resultsTitle, isPresented: $showingScore) {
+            Button("Continue", action: nextRound)
+        } message: {
+            Text("Your score is \(playerScore)")
+        }
+        .alert(resultsTitle, isPresented: $showingFinalScore) {
+            Button("Restart", action: restartGame)
+        } message: {
+            Text("Final score: \(playerScore)")
+        }
+    }
+    func nextRound() {
+        round += 1
+        computerChoice = Int.random(in: 0...2)
+        shouldWin.toggle()
+    }
+    
+    func restartGame() {
+        round = 1
+        playerScore = 0
+        cpuScore = 0
+        shouldWin.toggle()
+        computerChoice = Int.random(in: 0...2)
+    }
+    
+    func handleButtonPress(emoji: String) {
+        playerChoice = emoji
+        
+        if(outcome == .draw) {
+            resultsTitle = "Draw"
+        } else if ((outcome == .win && shouldWin) || outcome == .lose && !shouldWin) {
+            resultsTitle = "Correct"
+            playerScore += 1
+        } else {
+            resultsTitle = "Incorrect"
+            cpuScore += 1
+        }
+        
+        if round == 10 {
+            showingFinalScore = true
+            if playerScore > cpuScore {
+                resultsTitle = "You win!"
+            } else if playerScore == cpuScore {
+                resultsTitle = "You tied!"
+            } else {
+                resultsTitle = "You Lose!"
+            }
+        } else {
+            showingScore = true
+        }
     }
 }
 
